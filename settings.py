4ab3fafe62e0bb3c5f8dec439020ce1905c2e3b9
Peter-Vanderhyde <71889138+Peter-Vanderhyde@users.noreply.github.com>
2021-11-22 02:31:00 -0600

    Create settings.py
    Constant settings

---
+++ settings.py
@@ -0,0 +1,5 @@
+from colors import *
+RESOLUTION = (600, 600)
+IMAGE_PATH = "Images/"
+# [<name>, <alpha>, <colorkey>]
+ASSETS = (("player_body", True, white), ("player_bullet", True), ("barrel", True, white))
